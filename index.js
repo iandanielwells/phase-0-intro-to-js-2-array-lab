6e60eaa878dcd5e7ddebf272985cd4a413cc3f66
Ian <iandanielwells@gmail.com>
2022-12-14 13:53:49 -0500

    commit tree bacf46da6b418f50668ad1e0d00347a994fc40a7

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+
+function destructivelyAppendCat(name) {
+    cats.push(name)
+}
+function destructivelyPrependCat(name) {
+    cats.unshift(name)
+}
+function destructivelyRemoveLastCat() {
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+}
+
+function appendCat(name) {
+       let newCat = [...cats, name]
+    return newCat
+}
+
+function prependCat(name) {
+   let oldCat = [name, ...cats]
+   return oldCat
+}
+
+function removeLastCat(){
+    const newCat = [...cats];
+    newCat.pop()
+    return newCat
+}
+
+function removeFirstCat() {
+    const firstCat = [...cats];
+    firstCat.shift()
+    return firstCat
+}
